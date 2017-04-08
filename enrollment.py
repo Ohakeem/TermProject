3885a53e00a0e57e3c200a0d55b6107d0622f530
Ohakeem <omar.alhakeem@g.austincc.edu>
2017-04-07 23:32:53 -0500

    First commit

---
+++ enrollment.py
@@ -0,0 +1,11 @@
+from student import Student
+from course import Course
+from gradebook import Grade_book
+class Enrollment:
+    
+    def __init__(self, enroll_id, student, course):
+        self.enroll_id = enroll_id
+        self.student = student
+        self.course = course
+
+    
---
+++ student.py
@@ -0,0 +1,11 @@
+class Student:
+
+    def __init__(self, student_id, first_name, last_name, enroll_date):
+
+        self.__student_id = student_id
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__enroll_date = enroll_date
+    
+        
+    
